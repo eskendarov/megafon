0dc57d2ea187cea6832b1662f96f86561778c28c
EEA <envereskendarov@gmail.com>
2021-08-04 14:40:09 +0300

    Init commit

---
+++ src/test/java/ru/megafon/test/MegafonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.megafon.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegafonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
